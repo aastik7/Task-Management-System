c10e91e3ff49c178f5fed1d9541d56864005c091
Aastik <aastikdude7@gmail.com>
2024-01-27 23:54:31 +0530

    connecting to server

--- task manager.py
+++ task manager.py
@@ -1,1 +1,12 @@
 import mysql.connector #pip install mysql-connector-python
+from mysql.connector import errorcode
+
+
+# Connect to the MySQL database
+try:
+    conn = mysql.connector.connect(
+        host='sql_host',
+        user='mysql_user',
+        password='mysql_password',
+        database='task_manager'
+    )
